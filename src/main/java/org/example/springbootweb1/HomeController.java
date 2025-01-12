89bdaf9f87b4bf82f75839c2ebab9fa5faa4c197
IT22074072 <it22074072@my.sliit.lk>
2025-01-12 10:14:32 +0530

    Accepting data in the servlet way

--- src/main/java/org/example/springbootweb1/HomeController.java
+++ src/main/java/org/example/springbootweb1/HomeController.java
@@ -1,14 +1,24 @@
 package org.example.springbootweb1;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public String home(){
+    @RequestMapping("/") //this is the request
+    public String home(){  //call this method
         System.out.println("home");
         return "index.jsp";
+    }
+
+    @RequestMapping("/add")
+    public String add(HttpServletRequest req){
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+        System.out.println("result: " + result);
+        return "result.jsp";
     }
 }
