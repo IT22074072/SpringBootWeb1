41750d44d9309b68cb6c68f56f78d12bcd497fee
IT22074072 <it22074072@my.sliit.lk>
2025-01-12 10:50:41 +0530

    RequestParam

--- src/main/java/org/example/springbootweb1/HomeController.java
+++ src/main/java/org/example/springbootweb1/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -15,9 +16,7 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(HttpServletRequest req, HttpSession session){
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session){
         int result = num1 + num2;
         session.setAttribute("result", result);
         System.out.println("result: " + result);
