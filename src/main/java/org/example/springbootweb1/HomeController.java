f2629a36ddb6966ca9ba967422027badad74da0d
IT22074072 <it22074072@my.sliit.lk>
2025-01-12 12:52:46 +0530

    Model and view

--- src/main/java/org/example/springbootweb1/HomeController.java
+++ src/main/java/org/example/springbootweb1/HomeController.java
@@ -1,11 +1,9 @@
 package org.example.springbootweb1;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -17,10 +15,10 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){ //transfer between controller and the jsp - model
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv){ //transfer between controller and the jsp - model
         int result = num1 + num2;
-        model.addAttribute("result", result);
-        System.out.println("result: " + result);
-        return "result";
+        mv.addObject("result", result);
+        mv.setViewName("result");
+        return mv;
     }
 }
